2cb377616a1f4f9aa99b7111deb30e58116195b3
Super BianzZz <89448318+dreambabyyy@users.noreply.github.com>
2025-01-08 04:02:47 +0000

    Add files via upload

---
+++ config/banner.js
@@ -0,0 +1,32 @@
+function printBanner() {
+    // Header ASCII Art and Info
+    const printHeader = () => {
+        console.log(`
+╔═════════════════════════════════════════════════╗
+║                                                 ║
+██████╗ ██╗███╗   ██╗███████╗████████╗███████╗    ║
+██╔══██╗██║████╗  ██║██╔════╝╚══██╔══╝██╔════╝    ║
+██████╔╝██║██╔██╗ ██║█████╗     ██║   █████╗      ║
+██╔═══╝ ██║██║╚██╗██║██╔══╝     ██║   ██╔══╝      ║
+██║     ██║██║ ╚████║███████╗   ██║   ███████╗    ║
+╚═╝     ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚══════╝    ║
+║  SUPER BIANZZZ                                  ║
+║                                                 ║
+║                                                 ║
+║  JOIN TELEGRAM CHANNEL NOW!                     ║
+║  https://t.me/superbianz                        ║
+║  @SuperBianzZz - OFFICIAL                       ║
+║  CHANNEL                                        ║
+║                                                 ║
+║  FAST - RELIABLE - SECURE                       ║
+║                                                 ║
+║                                                 ║
+╚═════════════════════════════════════════════════╝
+        `);
+    };
+
+    // Call the printHeader function
+    printHeader();
+}
+
+module.exports = printBanner;
